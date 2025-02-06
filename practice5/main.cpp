b553436d6d7dd6bfe81abde0c904432d183744cd
Ivan Petrukhin <petruekhin@yandex.ru>
2025-02-06 17:18:43 +0300

    Task 2

--- practice5/main.cpp
+++ practice5/main.cpp
@@ -41,13 +41,16 @@ uniform mat4 projection;
 
 layout (location = 0) in vec3 in_position;
 layout (location = 1) in vec3 in_normal;
+layout (location = 2) in vec2 in_texcoord;
 
 out vec3 normal;
+out vec2 texcoord;
 
 void main()
 {
     gl_Position = projection * viewmodel * vec4(in_position, 1.0);
     normal = mat3(viewmodel) * in_normal;
+    texcoord = in_texcoord;
 }
 )";
 
@@ -55,13 +58,14 @@ const char fragment_shader_source[] =
 R"(#version 330 core
 
 in vec3 normal;
+in vec2 texcoord;
 
 layout (location = 0) out vec4 out_color;
 
 void main()
 {
     float lightness = 0.5 + 0.5 * dot(normalize(normal), normalize(vec3(1.0, 2.0, 3.0)));
-    vec3 albedo = vec3(1.0);
+    vec3 albedo = vec3(texcoord, 0.0);
     out_color = vec4(lightness * albedo, 1.0);
 }
 )";
@@ -172,6 +176,10 @@ int main() try
     glEnableVertexAttribArray(1);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, sizeof(obj_data::vertex),
                           (void *)(offsetof(obj_data::vertex, normal)));
+
+    glEnableVertexAttribArray(2);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(obj_data::vertex),
+                          (void *)(offsetof(obj_data::vertex, texcoord)));
 
     GLuint ebo;
     glGenBuffers(1, &ebo);
